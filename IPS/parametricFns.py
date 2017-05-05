853903a0aa1fa10f47d961e5405666f187abe069
alemc2 <alemc2@gmail.com>
2017-05-05 03:20:02 -0400

    Changes for IPS. Need to debug further

--- IPS/parametricFns.py
+++ IPS/parametricFns.py
@@ -1,11 +1,14 @@
 import numpy as np
 
 
+# a(Xn) = [kappa*(sigmaHat - sigma(n)) ,r*S1,..,r*SN]
 def A(portfolio, params):
     sigma, S = portfolio[:, :1], portfolio[:, 1:]
     return np.hstack((params['kappa'] * (params['sigmaHat'] - sigma), params['r'] * S))
 
 
+# b(Xn) =
+# [gamma*np.sqrt(sigma(n)),simga0*sigma(n)*S1,.....,simga0*sigma(n)*SN]
 def B(portfolio, params):
     sigma, S = portfolio[:, :1], portfolio[:, 1:]
     return np.hstack((params['gamma'] * np.sqrt(sigma), params['sigma0'] * sigma * S))
